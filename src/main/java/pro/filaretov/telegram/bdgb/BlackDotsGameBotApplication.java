1b51d53dae96ea0f0f88fc558a8df5e4560c5e03
Petr Filaretov <petr.filaretov@gmail.com>
2020-09-02 10:07:17 +0300

    initial commit

---
+++ src/main/java/pro/filaretov/telegram/bdgb/BlackDotsGameBotApplication.java
@@ -0,0 +1,13 @@
+package pro.filaretov.telegram.bdgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackDotsGameBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackDotsGameBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/filaretov/telegram/bdgb/BlackDotsGameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.filaretov.telegram.bdgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackDotsGameBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
