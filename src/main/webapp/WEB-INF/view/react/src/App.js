a034fe8e1aa3d47ac19ff584b785524950cdb273
Petr Filaretov <petr.filaretov@gmail.com>
2020-09-09 10:36:54 +0300

    add AGH game front-end, mvn builds fine

---
+++ src/main/webapp/WEB-INF/view/react/src/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './components/Canvas';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.shoot = this.shoot.bind(this);
+    this.socket = null;
+  }
+
+  componentDidMount() {
+    const self = this;
+
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+
+    window.onresize = () => {
+      const cnv = document.getElementById('aliens-go-home-canvas');
+      cnv.style.width = `${window.innerWidth}px`;
+      cnv.style.height = `${window.innerHeight}px`;
+    };
+    window.onresize();
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  shoot() {
+    this.props.shoot(this.canvasMousePosition);
+  }
+
+  render() {
+    return (
+      <Canvas
+        angle={this.props.angle}
+        gameState={this.props.gameState}
+        startGame={this.props.startGame}
+        trackMouse={event => (this.trackMouse(event))}
+        shoot={this.shoot}
+      />
+    );
+  }
+}
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+    position: PropTypes.shape({
+      x: PropTypes.number.isRequired,
+      y: PropTypes.number.isRequired
+    }).isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
+  moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+  shoot: PropTypes.func.isRequired,
+};
+
+App.defaultProps = {
+};
+
+export default App;
---
+++ src/main/webapp/WEB-INF/view/react/src/components/Canvas.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
+import CurrentScore from './CurrentScore'
+import FlyingObject from './FlyingObject';
+import StartGame from './StartGame';
+import Title from './Title';
+import CannonBall from './CannonBall';
+import Heart from './Heart';
+
+const Canvas = (props) => {
+  const gameHeight = 1200;
+  const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
+
+  const lives = [];
+  for (let i = 0; i < props.gameState.lives; i++) {
+    const heartPosition = {
+      x: -180 - (i * 70),
+      y: 35
+    };
+    lives.push(<Heart key={i} position={heartPosition}/>);
+  }
+
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+      onClick={props.shoot}
+    >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+      <Sky />
+      <Ground />
+
+      {props.gameState.cannonBalls.map(cannonBall => (
+        <CannonBall
+          key={cannonBall.id}
+          position={cannonBall.position}
+        />
+      ))}
+
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CurrentScore score={props.gameState.kills} />
+
+      { ! props.gameState.started &&
+      <g>
+        <StartGame onClick={() => props.startGame()} />
+        <Title />
+      </g>
+      }
+
+      {props.gameState.flyingObjects.map(flyingObject => (
+        <FlyingObject
+          key={flyingObject.id}
+          position={flyingObject.position}
+        />
+      ))}
+
+      {lives}
+    </svg>
+  );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+  shoot: PropTypes.func.isRequired,
+};
+
+Canvas.defaultProps = {
+};
+
+export default Canvas;
