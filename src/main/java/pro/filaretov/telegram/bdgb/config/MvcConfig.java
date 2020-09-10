f44e8ff5e62ba3e8bb5bd8468d87645be6d39157
Petr Filaretov <petr.filaretov@gmail.com>
2020-09-10 11:11:27 +0300

    AGH game works with spring-boot start

---
+++ src/main/java/pro/filaretov/telegram/bdgb/config/MvcConfig.java
@@ -0,0 +1,25 @@
+package pro.filaretov.telegram.bdgb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Wev MVC configuration.
+ */
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public static final String REACT_BUILD_DIR = "/WEB-INF/view/react/build/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(REACT_BUILD_DIR + "static/");
+        registry.addResourceHandler("/*.js").addResourceLocations(REACT_BUILD_DIR);
+        registry.addResourceHandler("/*.json").addResourceLocations(REACT_BUILD_DIR);
+        registry.addResourceHandler("/*.ico").addResourceLocations(REACT_BUILD_DIR);
+        registry.addResourceHandler("/index.html").addResourceLocations(REACT_BUILD_DIR + "index.html");
+    }
+}
